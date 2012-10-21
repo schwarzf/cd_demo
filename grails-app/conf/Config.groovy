3f0bb6017cdad6eac70e403f6d2a5158dafe9899
Florian Schwarz <florian.schwarz2@gmail.com>
2012-10-21 19:37:07 +0200

    fixed logging

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -77,6 +77,8 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
+	'null' name:'stacktrace'
+	
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
            'org.codehaus.groovy.grails.web.sitemesh',       // layouts
